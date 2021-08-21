27d3c232cfd2f446f962f309e6edab0a46c6767a
Holger Hinzberg <holger@hinzberg.de>
2021-08-21 21:17:54 +0200

    Initial Commit

---
+++ IO/HHFileHelperMac.swift
@@ -0,0 +1,15 @@
+//  HHSFileHelperMac.swift
+//  Created by Holger Hinzberg on 12.02.18.
+//  Copyright © 2018 Holger Hinzberg. All rights reserved.
+
+import Foundation
+
+class HHFileHelperMac: NSObject
+{
+    public func getDesktopUrl() -> URL
+    {
+        let paths = NSSearchPathForDirectoriesInDomains(.desktopDirectory, .userDomainMask, true)
+        //let desktopUrl = FileManager.URLsForDirectory(.desktopDirectory, inDomains: .UserDomainMask).first as! NSURL
+        return URL.init(fileURLWithPath: paths[0])
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+#  Apple Extension Library
+## Helper Classes für  macOS / iOS 
+
+2021-08
+- Added Array + Helper.swift
+- Added Color + Helper.swift
+
+2018-05-17
+- First Commit
